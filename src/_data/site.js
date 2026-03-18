ea5725c67beda3ccea8043d472f0908c1a5414e3
Kumar Anirudha <mail@anirudha.dev>
2026-03-18 18:52:06 +0530

    add: download tracking in google analytics

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,6 +5,7 @@ export default {
   url: "https://orionpod.com",
   image: "/assets/images/splash.png",
   twitterHandle: "@kranirudha",
+  // TODO: Replace Google Drive link with a Dub.co short link for download tracking
   downloadUrl: "https://drive.google.com/file/d/1nYDi_AlKow1X1omAqACMISZp3eMjyIdS/view?usp=drive_link",
   twitter: "https://x.com/kranirudha",
   mastodon: "https://fosstodon.org/@ani",
