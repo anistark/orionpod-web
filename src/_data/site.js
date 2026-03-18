fde145754b6fd90ebe61c1c6254fe031a25cbacf
Kumar Anirudha <mail@anirudha.dev>
2026-03-18 14:52:24 +0530

    Update domain to orionpod.com and add download link

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,8 +2,8 @@ export default {
   title: "OrionPod",
   description:
     "A lightweight desktop runtime for running AI models locally. No cloud. No heavy infra. Just your machine.",
-  url: "https://orionpod.anirudha.dev",
-  downloadUrl: "#",
+  url: "https://orionpod.com",
+  downloadUrl: "https://drive.google.com/file/d/1nYDi_AlKow1X1omAqACMISZp3eMjyIdS/view?usp=drive_link",
   twitter: "https://x.com/kranirudha",
   mastodon: "https://fosstodon.org/@ani",
   year: new Date().getFullYear(),
